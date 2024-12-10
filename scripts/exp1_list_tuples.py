fef1a49f61595194e312c40ae578152c2107c670
Edwin Carreño <ecarrenolozano@gmail.com>
2024-12-10 17:37:50 +0100

    refactor: add arguments to the function create_lists

--- scripts/exp1_list_tuples.py
+++ scripts/exp1_list_tuples.py
@@ -1,13 +1,14 @@
+import ast
 import cProfile
 import csv
 import os
 import pstats
 
+import networkx as nx
 from pympler import asizeof
 from tabulate import tabulate
 
-
-FILE_PATH_DATASETS = "../../../DATASETS"
+FILE_PATH_DATASETS = "data_examples"
 
 
 # ----------------  HELPER FUNCTIONS
@@ -15,13 +16,12 @@ def profile_objects_pympler(**kwargs):
     sizes_mebibytes = []
     for object in kwargs.keys():
         size_summary = asizeof.asized(kwargs[object], detail=0).format()
-        total_size_mebibytes = float(
-            size_summary.split(" ")[-2].split("=")[1]
-        ) / (1024**2)
 
-        flat_size_mebibytes = float(
-            size_summary.split(" ")[-1].split("=")[1]
-        ) / (1024**2)
+        total_size_mebibytes = float(size_summary.split(" ")[-2].split("=")[1])
+        total_size_mebibytes /= 1024**2
+
+        flat_size_mebibytes = float(size_summary.split(" ")[-1].split("=")[1])
+        flat_size_mebibytes /= 1024**2
 
         sizes_mebibytes.append(
             [
@@ -65,21 +65,79 @@ def load_csv_generator(file_path, header=True):
             yield tuple(row)
 
 
-def generate_lists(nodes_generator, edges_generator):
-    list_nodes = [item for item in nodes_generator]
-    list_edges = [item for item in edges_generator]
+def create_lists(file_path_nodes, file_path_edges, header_nodes=True, header_edges=True):
+
+    nodes_ds = load_csv_generator(file_path_nodes, header=header_nodes)
+    edges_ds = load_csv_generator(file_path_edges, header=header_edges)
+
+    list_nodes = [item for item in nodes_ds]
+    list_edges = [item for item in edges_ds]
 
     return list_nodes, list_edges
+
+
+def to_networkx_nodes_format(nodes_iterable, mapping_properties=True):
+    for node_id, node_label, properties in nodes_iterable:
+        # Original format
+        #  (node id, node label, properties)
+
+        # Desired format
+        #  (node id, properties as dict)
+
+        properties = ast.literal_eval(properties)
+
+        if mapping_properties:
+            properties["node_label"] = node_label
+
+        yield (node_id, properties)
+
+
+def to_networkx_edges_format(edges_iterable, mapping_properties=True):
+    for edge_tuple in edges_iterable:
+        edge_id, source_id, target_id, edge_label, properties = edge_tuple
+
+        # Original format
+        #  (edge id, source (edge id), target (edge id), label, properties)
+
+        # Desired format
+        #  (source (edge id), target (edge id), properties as dict)
+
+        properties = ast.literal_eval(properties)
+
+        if mapping_properties:
+            properties["edge_id"] = edge_id
+            properties["edge_label"] = edge_label
+
+        yield (
+            source_id,
+            target_id,
+            properties,
+        )
+
+
+def networkx_graph_from_lists(nodes_iterable, edges_iterable, graph_type=nx.DiGraph()):
+    # Create an empty graph
+    networkx_graph = graph_type
+
+    # Populate the graph with edges and their properties
+    networkx_graph.add_edges_from(edges_iterable)
+
+    # Populate the graph with nodes and their properties
+    networkx_graph.add_nodes_from(nodes_iterable)
+
+    return networkx_graph
 
 
 # ----------------  MAIN FUNCTION
 def main():
 
-    file_name_nodes = "dataset_300_nodes_proteins.csv"
-    file_name_edges = "dataset_300_edges_interactions.csv"
+    filename_nodes = "dataset_30000_nodes_proteins.csv"
+    filename_edges = "dataset_30000_edges_interactions.csv"
 
-    file_path_nodes = os.path.join(FILE_PATH_DATASETS, file_name_nodes)
-    file_path_edges = os.path.join(FILE_PATH_DATASETS, file_name_edges)
+    file_path_nodes = os.path.join(FILE_PATH_DATASETS, filename_nodes)
+    file_path_edges = os.path.join(FILE_PATH_DATASETS, filename_edges)
+
+    print(os.getcwd())
 
     # ------------------  TIME STATS  -------------------------
     print("======   Time profile")
@@ -87,9 +145,8 @@ def main():
     profiler = cProfile.Profile()
     profiler.enable()
 
-    ds_list_nodes, ds_list_edges = generate_lists(
-        load_csv_generator(file_path_nodes),
-        load_csv_generator(file_path_edges),
+    ds_list_nodes, ds_list_edges = create_lists(
+        file_path_nodes, file_path_edges, header_nodes=True, header_edges=True
     )
 
     profiler.disable()
